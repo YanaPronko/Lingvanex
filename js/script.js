0daeb5156da51c00fb06120d05c08169d882ed46
Yana Pronko <yaniar@tut.by>
2024-04-21 02:12:32 +0300

    feat: add tabs, last section

--- js/script.js
+++ js/script.js
@@ -1,5 +1,47 @@
 'use strict';
 
 window.addEventListener('DOMContentLoaded', () => {
-  
+  const tabsNavigation = document.querySelector('.tabs__navigation');
+  const tabsElements = document.querySelectorAll('.tabs__element');
+
+
+  if (tabsNavigation) {
+    tabsNavigation.addEventListener('click', handleTabsClick);
+  }
+
+  function handleTabsClick(e) {
+    const activeTabsButton = e.target.closest('.tabs__item');
+    const tabsButtons = tabsNavigation.querySelectorAll('.tabs__item');
+
+    if (activeTabsButton && tabsButtons) {
+      tabsButtons.forEach((tab, index) => {
+        tab.classList.remove('tabs__item--active');
+        if (tab === activeTabsButton) {
+          activeTabsButton.classList.add('tabs__item--active');
+          hideTabs();
+          showTabs(index);
+
+        }
+      });
+    }
+  }
+
+  function hideTabs() {
+    if (tabsElements) {
+      tabsElements.forEach(tab => {
+        tab.hidden = true;
+        tab.classList.remove('fade');
+      });
+    }
+  }
+
+  function showTabs(index) {
+    if (tabsElements) {
+      tabsElements[index].hidden = false;
+      tabsElements[index].classList.add('fade');
+    };
+  }
+
+
 });
+
